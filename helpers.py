2290372c7b09ccd0b2ea62647f3fcf67f2aea836
Hasib  Zunair <hasibzunair@gmail.com>
2019-09-24 22:06:00 -0400

    added data loader

---
+++ helpers.py
@@ -0,0 +1,85 @@
+import pylab
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Declaring constants
+FIG_WIDTH=20 # Width of figure
+HEIGHT_PER_ROW=3 # Height of each row when showing a figure which consists of multiple rows
+RESIZE_DIM=28 # The images will be resized to 28x28 pixels
+
+
+
+def show_images(images, cols = 1, titles = None, save_fig = "default"):
+    """Display a list of images in a single figure with matplotlib.
+    
+    Parameters
+    ---------
+    images: List of np.arrays compatible with plt.imshow.
+    
+    cols (Default = 1): Number of columns in figure (number of rows is 
+                        set to np.ceil(n_images/float(cols))).
+    
+    titles: List of titles corresponding to each image. Must have
+            the same length as titles.
+            
+    USAGE: show_images(normal_images[:20], cols = 4, titles = None, save_fig="Images")
+    """
+    assert((titles is None)or (len(images) == len(titles)))
+    n_images = len(images)
+    if titles is None: titles = ['Image (%d)' % i for i in range(1,n_images + 1)]
+    fig = plt.figure()
+    #fig.set_title("Samples of infected red blood cells")
+    
+    for n, (image, title) in enumerate(zip(images, titles)):
+        a = fig.add_subplot(cols, np.ceil(n_images/float(cols)), n + 1)
+        if image.ndim == 2:
+            plt.gray()
+        plt.imshow(image)
+        plt.axis("off")
+        a.set_title(title)
+    fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
+    #plt.savefig('{}'.format(save_fig), dpi=50)
+    plt.show()
+
+
+
+def imshow_group(X,y,y_pred=None,n_per_row=10,phase='processed'):
+    '''helper function to visualize a group of images along with their categorical true labels (y) and prediction probabilities.
+    Args:
+        X: images
+        y: categorical true labels
+        y_pred: predicted class probabilities
+        n_per_row: number of images per row to be plotted
+        phase: If the images are plotted after resizing, pass 'processed' to phase argument. 
+            It will plot the image and its true label. If the image is plotted after prediction 
+            phase, pass predicted class probabilities to y_pred and 'prediction' to the phase argument. 
+            It will plot the image, the true label, and it's top 3 predictions with highest probabilities.
+            
+            
+    USAGE: imshow_group(inp_feat, g_t)
+    '''
+    n_sample=len(X)
+    img_dim=X.shape[1]
+    j=np.ceil(n_sample/n_per_row)
+    fig=plt.figure(figsize=(FIG_WIDTH,HEIGHT_PER_ROW*j))
+    for i,img in enumerate(X):
+        plt.subplot(j,n_per_row,i+1)
+#         img_sq=np.squeeze(img,axis=2)
+#         plt.imshow(img_sq,cmap='gray')
+        plt.imshow(img)
+        if phase=='processed':
+            plt.title(y[i]) #np.argmax(y[i])
+        if phase=='prediction':
+            top_n=3 # top 3 predictions with highest probabilities
+            ind_sorted=np.argsort(y_pred[i])[::-1]
+            h=img_dim+4
+            for k in range(top_n):
+                string='pred: {} ({:.0f}%)\n'.format(ind_sorted[k],y_pred[i,ind_sorted[k]]*100)
+                plt.text(img_dim/2, h, string, horizontalalignment='center',verticalalignment='center')
+                h+=4
+            if y is not None:
+                plt.text(img_dim/2, -4, 'true label: {}'.format(np.argmax(y[i])), 
+                         horizontalalignment='center',verticalalignment='center')
+        plt.axis('off')
+    plt.show()
